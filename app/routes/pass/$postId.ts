73ad10fcc95aa5390c3a3c7a4cd3cc426761525e
Justin Robb <reptarsrage@gmail.com>
2023-01-07 17:14:02 -0800

    Adding api routes for smash/pass

---
+++ app/routes/pass/$postId.ts
@@ -0,0 +1,27 @@
+import type { ActionArgs } from "@remix-run/node"; // or cloudflare/deno
+import { json } from "@remix-run/node"; // or cloudflare/deno
+import { hasPost, pass } from "~/models/post.server";
+
+export const action = async ({ request, params }: ActionArgs) => {
+  if (request.method !== "PATCH") {
+    throw json("Method Not Supported", { status: 415 });
+  }
+
+  const postId = params.postId;
+  if (!postId) {
+    throw json("Bad Request", { status: 400 });
+  }
+
+  const id = parseInt(postId, 10);
+  if (isNaN(id)) {
+    throw json("Bad Request", { status: 400 });
+  }
+
+  const exists = await hasPost(id);
+  if (!exists) {
+    throw json("Not Found", { status: 404 });
+  }
+
+  const post = await pass(id);
+  return json(post, 200);
+};
