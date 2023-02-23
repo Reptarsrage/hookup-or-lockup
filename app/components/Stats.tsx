61a79643b5770dec23dab0f6010090d86f27ea85
Justin Robb <reptarsrage@gmail.com>
2023-02-22 22:08:36 -0800

    feat: adding results screen

--- app/components/Stats.tsx
+++ app/components/Stats.tsx
@@ -1,36 +1,39 @@
 import clsx from "clsx";
-import { useSpring, animated } from "@react-spring/web";
 
 import type { PostWithImageAndStats } from "~/models/post.server";
 import Number from "~/components/Number";
+import { Graph } from "./Graph";
 
 interface StatsProps {
-  post: PostWithImageAndStats | null;
+  post: PostWithImageAndStats;
+  shown: boolean;
 }
 
-function Stats({ post }: StatsProps) {
-  const maxWidth = 100;
-  const smashes = post?.smashes ?? 0;
-  const passes = post?.passes ?? 0;
-  const totalVotes = post?.totalVotes ?? 1;
+export interface GraphProps {
+  width: number;
+  votes: number;
+  total: number;
+  lockedUp: boolean;
+}
 
-  const passesStyle = useSpring({
-    from: { width: 0 },
-    to: { width: (passes / totalVotes) * maxWidth },
-  });
+function Stats({ post, shown }: StatsProps) {
+  const maxWidth = 128;
+  const smashes = post.smashes;
+  const passes = post.passes;
+  const totalVotes = post.totalVotes;
 
-  const smashesStyle = useSpring({
-    from: { width: 0 },
-    to: { width: (smashes / totalVotes) * maxWidth },
-  });
-
-  if (!post) {
-    return null;
+  function revealWithShown(otherClasses?: string) {
+    return clsx(
+      otherClasses,
+      "transition-opacity duration-300",
+      !shown && "opacity-0"
+    );
   }
 
   return (
     <div className="flex flex-col items-center justify-center gap-2 p-4">
-      <div>
+      {/* Top text */}
+      <span className={revealWithShown()}>
         What others chose for{" "}
         <span
           className={clsx(
@@ -41,39 +44,66 @@ function Stats({ post }: StatsProps) {
           {post.title}
         </span>
         {"..."}
-      </div>
+      </span>
+
       <div className="flex w-full justify-center gap-4">
-        <div className="flex flex-1 flex-col items-end gap-1 pt-2 text-right">
-          <div>Oh No!</div>
-          <animated.div
-            style={passesStyle}
-            className="h-8 rounded bg-blue-dark"
-          />
-          <div>
-            <Number value={post.passes} />
+        {/* Graph left */}
+        {shown && (
+          <div
+            className="flex flex-1 flex-col items-end gap-1 pt-2 text-right"
+            style={{ width: maxWidth }}
+          >
+            <div>Oh No!</div>
+            <Graph
+              votes={passes}
+              total={totalVotes}
+              width={maxWidth}
+              lockedUp
+            />
+            <div>
+              <Number value={post.passes} />
+            </div>
           </div>
-        </div>
+        )}
+
+        {/* Profile image */}
         <div
           className={clsx(
             "relative h-32 w-32 overflow-hidden rounded border-4",
-            post.lockedUp ? "border-blue-dark" : "border-red"
+            shown && (post.lockedUp ? "border-blue-dark" : "border-red")
           )}
         >
           <img
-            className="absolute h-full w-full opacity-80"
-            src={post.lockedUp ? "/oh-no.svg" : "/ay-yo.svg"}
-            alt={post.lockedUp ? "Oh No" : "Ay Yo"}
+            className="h-full w-full object-cover"
+            src={post.image}
+            alt={post.title}
           />
-          <img src={post.image} alt={post.title} />
         </div>
-        <div className="flex flex-1 flex-col gap-1 pt-2">
-          <div>Ay Yo!</div>
-          <animated.div style={smashesStyle} className="h-8 rounded bg-red" />
-          <div>
-            <Number value={post.smashes} />
+
+        {/* Graph right */}
+        {shown && (
+          <div
+            className="flex flex-1 flex-col gap-1 pt-2"
+            style={{ width: maxWidth }}
+          >
+            <div>Ay Yo!</div>
+            <Graph
+              votes={smashes}
+              total={totalVotes}
+              width={maxWidth}
+              lockedUp={false}
+            />
+            <div>
+              <Number value={post.smashes} />
+            </div>
           </div>
-        </div>
+        )}
       </div>
+
+      {/* Bottom text */}
+      <span className={revealWithShown()}>
+        <Number value={totalVotes} /> total votes
+      </span>
     </div>
   );
 }
